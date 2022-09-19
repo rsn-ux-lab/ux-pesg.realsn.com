2067bb1167bed64cd2f6168ae4b42a817fd72eed
장지훈 <me2designer@realsn.com>
2022-09-19 15:57:15 +0900

    index 모달 작업 ing

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -5,14 +5,13 @@
     </SectionSubTitle>
     <section class="gallery">
       <div class="inner">
-        <h2 class="gallery__title">P.ESG는 빅데이터로 소비자 인식요인에 접근한 지표입니다.</h2>
-        <p class="gallery__desc">
-          P.ESG는 기업 ESG에 대한 우리 사회의 반응에 측정하여<br />
-          기업 ESG 활동의 목적이 소비자 인식 효과에 있음을 나타내고자 합니다.
-        </p>
-        <button class="gallery-button">
-          <img class="gallery-button__thumb" src="" alt="" />
-        </button>
+        <header class="gallery-header">
+          <h2 class="gallery-header__title">우리는 Web에서 빅데이터를 모아<br />기업 ESG 노출 활동을 분석하는 새로운 Index를 개발했습니다.</h2>
+          <p class="gallery-header__desc">P.ESG는 웹 상의 기업 ESG 이슈를 자동수집, 딥러닝 AI로 ESG를 분류하고 평가합니다.</p>
+        </header>
+        <main class="gallery-body">
+          <button class="gallery-modal-button" :style="{ backgroundImage: `url(${require('@/assets/images/index/gallery_thumb/thumb1.jpg')})` }"></button>
+        </main>
       </div>
     </section>
   </main>
@@ -39,5 +38,39 @@ export default {
 <style lang="scss" scoped>
 #container {
   background-color: #f1f7fc;
+  .gallery {
+    $g: &;
+
+    &-header {
+      padding-top: 10.5rem;
+      &__title {
+        font-size: 3.5rem;
+        font-weight: bold;
+        text-align: center;
+        margin-bottom: 2rem;
+      }
+      &__desc {
+        margin-bottom: 5.8rem;
+        font-size: 2rem;
+        color: #514888;
+        text-align: center;
+        font-weight: 500;
+      }
+    }
+    &-body {
+      padding-bottom: 6rem;
+      .gallery-modal-button {
+        overflow: hidden;
+        position: relative;
+        display: block;
+        width: 108rem;
+        height: 56rem;
+        margin: 0 auto;
+        border-radius: $G-box-radius;
+        background-color: #d2d5d9;
+        box-shadow: $G-box-shadow;
+      }
+    }
+  }
 }
 </style>
--- src/views/main/VisualThumb.vue
+++ src/views/main/VisualThumb.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="visual-thumb" appear>
-    <div class="visual-thumb"></div>
+    <div class="visual-thumb" :style="{ backgroundImage: `url(${require('@/assets/images/main/visual_thumb.jpg')})` }"></div>
   </transition>
 </template>
 
@@ -22,7 +22,9 @@ export default {
     border: 0.1rem solid #fff;
     border-radius: 2rem;
     background-color: rgba(#fff, 0.3);
+    background-size: cover;
     transform: translate(39.5rem, -50%);
+    box-shadow: $G-box-shadow;
   }
 }
 </style>
